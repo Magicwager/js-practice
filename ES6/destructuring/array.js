2ef9e351cbfddf39ac17b8f5212cc30f79339e0d
jian.wu <jian.wu@ishansong.com>
2018-05-05 17:39:57 +0800

    数组结构解析

--- ES6/destructuring/array.js
+++ ES6/destructuring/array.js
@@ -7,3 +7,17 @@ console.log(a,b,c)//1 2 3
 let [d,...e] = [1,2,3,4]
 console.log(d,e)//1 [2,3,4]
 
+
+//默认值解构赋值
+let [f=2]=[];
+let [g,h='world']=['hello'];
+console.log(f,g,h);//2 'hello' 'world'
+//默认值之中，只有严格赋值为undefined才能默认值生效
+let [x = 1] = [undefined];// 1
+let [z = 1] = [null];// null
+
+//如果默认值是表达式，那么这个表达式是惰性求值的，即只有在用到时才会执行表达式
+function fun(){
+    console.log("this is fun")
+}
+let[y=fun()]=[1]//不执行fun
