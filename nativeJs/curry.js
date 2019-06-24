b6809394b175354c47f8123bc62365b85ae65ac2
wujiank <wujiank@yonyou.com>
2019-06-24 22:34:49 +0800

    柯里化

---
+++ nativeJs/curry.js
@@ -0,0 +1,56 @@
+/**
+ * 实现一个add方法，使计算结果能够满足如下预期：
+ * add(1)(2)(3) ()
+ * add(1, 2, 3)(4)()
+ */
+
+/* function curryAdd() {
+    // 第一次执行时，定义一个数组专门用来存储所有的参数
+    let _args = [].slice.call(arguments);
+
+    // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
+    let adder = function () {
+
+        let _adder = function() {
+            // [].push.apply(_args, [].slice.call(arguments));
+            _args.push(...arguments);
+            return _adder;
+        };
+
+        // 利用隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
+        _adder.toString = function () {
+            return _args.reduce(function(x,y){
+                return x+y
+            });
+        }
+
+        return _adder;
+    }
+    // return adder.apply(null, _args);
+    return adder(..._args);
+}
+
+console.log(curryAdd(1)(2)(3))
+console.log(curryAdd(1, 2, 3)(4)()) 
+console.log(curryAdd(1)(2)) */
+
+/**
+ * 函数隐式转换，如果一个函数参与计算，则默认会调用函数的toString方法转为字符串进行计算，而如果重新定义toString方法，或定义valueOf的方法，
+ */
+ let fn = function(){
+     let arg = [...arguments];
+     let a = function(){
+         arg.push([...arguments])
+        return a
+     }
+     a.valueOf = function(){
+         return arg.reduce(function(x,y){
+            return x+y;
+         })
+     }
+     return a;
+ }
+ console.log(fn(1)(2)(3))
+
+
+
--- nativeJs/jstest.js
+++ nativeJs/jstest.js
@@ -1,4 +1,4 @@
-function Foo(){
+/* function Foo(){
     getName = function(){console.log("1")};
     return this;
 }
@@ -29,4 +29,42 @@ var ss="s";
 function ss(){
     console.log("ss")
 }
-console.log(ss)
+console.log(ss) */
+
+
+/**
+ * 实现一个add方法，使计算结果能够满足如下预期：
+ * add(1)(2)(3) ()
+ * add(1, 2, 3)(4)()
+ */
+
+ function add(param1){
+    return function(param2){
+        return function (param3){
+            return function (){
+                return param1+param2+param3;
+            }
+        }
+    }
+ }
+
+ function createCurry(func, args) {
+    
+        var arity = func.length;
+        var args = args || [];
+    
+        return function() {
+            var _args = [].slice.call(arguments);
+            [].push.apply(_args, args);
+    
+            // 如果参数个数小于最初的func.length，则递归调用，继续收集参数
+            if (_args.length < arity) {
+                return createCurry.call(this, func, _args);
+            }
+    
+            // 参数收集完毕，则执行func
+            return func.apply(this, _args);
+        }
+}
+
+
